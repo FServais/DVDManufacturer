8d30511957be869663630ded79a5b1d116af9f2b
Fabrice Servais <fabrice.servais@gmail.com>
2015-03-23 11:11:27 +0100

    Test

---
+++ Project 1/Tests/SecretKeySpec/src/Main.java
@@ -0,0 +1,32 @@
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import com.sun.org.apache.xml.internal.security.utils.Base64;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			String content = "azertyuiop";
+			
+			Mac mac = Mac.getInstance("HmacSHA1");
+			SecretKeySpec secret = new SecretKeySpec("password".getBytes(), mac.getAlgorithm());
+			mac.init(secret);
+			
+			byte[] digest = mac.doFinal(content.getBytes());
+			System.out.println("Hash : " + Base64.encode(digest));
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
