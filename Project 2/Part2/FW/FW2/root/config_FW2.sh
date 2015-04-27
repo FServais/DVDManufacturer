2d941886d327b1eafacf775f659b79b7969dde16
Fabrice Servais <fabrice.servais@gmail.com>
2015-04-27 02:28:18 +0200

    Update

--- Project 2/Part2/FW/FW2/root/config_FW2.sh
+++ Project 2/Part2/FW/FW2/root/config_FW2.sh
@@ -26,7 +26,7 @@ iptables -t nat -A PREROUTING -j DNAT -p tcp -d 172.14.5.2 --dport 53 --to-desti
 #filter
 #zone 6 incoming
 
-# Communication between DHCP_R2 and DHCP 
+# Communication between DHCP_R2 and DHCP
 iptables -t filter -A FORWARD -p udp -s 172.16.5.2 --sport 68 -d 172.16.6.2 --dport 67  -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -t filter -A FORWARD -p udp -s 172.16.5.2 --sport 67 -d 172.16.6.2 --dport 67  -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -t filter -A FORWARD -p udp -s 172.16.5.2 --sport 68 -d 172.16.6.2 --dport 67  -m state --state NEW,ESTABLISHED -j ACCEPT
@@ -34,8 +34,9 @@ iptables -t filter -A FORWARD -p udp -s 172.16.5.2 --sport 68 -d 172.16.6.2 --dp
 # SSH
 iptables -t filter -A FORWARD -p tcp -d 172.16.6.3 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 
-# HTTP 
+# HTTP
 iptables -t filter -A FORWARD -p tcp -s 172.16.5.0/24 -d 172.16.6.4 --dport 3128 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -t filter -A FORWARD -p tcp -s 172.14.6.2 -d 172.16.6.4 --dport 3128 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -t filter -A FORWARD -p tcp -s 172.16.5.0/24 -d 172.16.6.4 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -t filter -A FORWARD -p tcp -s 172.16.5.0/24 -d 172.16.6.4 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
 
@@ -47,10 +48,16 @@ iptables -t filter -A FORWARD -p tcp -s 172.14.6.2 -d 172.16.6.4 --dport 443 -m 
 iptables -t filter -A FORWARD -p udp -s 172.16.5.0/24 -d 172.16.6.5 --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 # Deny otherwise
-iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : "
-iptables -t filter -A FORWARD -j DROP
+iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : " -d 172.16.6.2
+iptables -t filter -A FORWARD -j DROP -d 172.16.6.2
+iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : " -d 172.16.6.3
+iptables -t filter -A FORWARD -j DROP -d 172.16.6.3
+iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : " -d 172.16.6.4
+iptables -t filter -A FORWARD -j DROP -d 172.16.6.4
+iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : " -d 172.16.6.5
+iptables -t filter -A FORWARD -j DROP -d 172.16.6.5
 
-#zone 6 outgoing 
+#zone 6 outgoing
 # SSH
 iptables -t filter -A FORWARD -p tcp -s 172.16.6.3 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 
@@ -71,11 +78,17 @@ iptables -t filter -A FORWARD -s 172.16.6.3 -d 172.14.6.2 -m state --state NEW,E
 iptables -t filter -A FORWARD -s 172.16.6.4 -d 172.14.6.2 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 # Deny otherwise
-iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : "
-iptables -t filter -A FORWARD -j DROP
+iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : " -s 172.16.6.2
+iptables -t filter -A FORWARD -j DROP -s 172.16.6.2
+iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : " -s 172.16.6.3
+iptables -t filter -A FORWARD -j DROP -s 172.16.6.3
+iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : " -s 172.16.6.4
+iptables -t filter -A FORWARD -j DROP -s 172.16.6.4
+iptables -t filter -A FORWARD -j LOG --log-prefix "[FORW] DROP : " -s 172.16.6.5
+iptables -t filter -A FORWARD -j DROP -s 172.16.6.5
 
 #zone 5 outgoing
-# U2 may access web 
+# U2 may access web
 iptables -t filter -A FORWARD -p tcp -s 172.16.5.0/24 -d 172.14.6.3 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -t filter -A FORWARD -p tcp -s 172.16.5.0/24 -d 172.14.6.3 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
 
